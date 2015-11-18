8d47d766e978393f8ac779c16582c460eebfafb7
raineryue <jinlei_yue@sina.com>
2015-11-18 17:52:35 +0800

    搭建静态界面基本控件元素
    搭建静态界面基本控件元素

--- 20151117001-CrazyGuessPicture/ViewController.m
+++ 20151117001-CrazyGuessPicture/ViewController.m
@@ -16,12 +16,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
 }
 
 @end
